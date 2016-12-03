585d8934b5152980796fa3855bdd11898ba0391c
rayamerico <rayamericoflores@gmail.com>
2016-12-02 19:30:44 -0500

    creando el objeto blog

---
+++ modelo/Objeto_Blog.php
@@ -0,0 +1,83 @@
+<?php 
+
+	class Objeto_Blog{
+
+//Propiedades del objeto blog
+		private $id;
+		private $titulo;
+		private $fecha;
+		private $comentario;
+		private $imagen;
+
+//Métodos de acceso
+		public function getId(){
+
+			return $this->id;
+
+		}
+
+		public function setId($id){
+
+			$this->id = $id;
+
+		}
+
+
+		public function getTitulo(){
+
+			return $this->titulo;
+
+		}
+
+		public function setTitulo($titulo){
+
+			$this->titulo = $titulo;
+
+		}
+
+
+		public function getFecha(){
+
+			return $this->fecha;
+
+		}
+
+		public function setFecha($fecha){
+
+			$this->fecha = $fecha;
+
+		}
+
+
+		public function getComentario(){
+
+			return $this->comentario;
+
+		}
+
+		public function setComentario($comentario){
+
+			$this->comentario = $comentario;
+
+		}
+
+
+		public function getImagen(){
+
+			return $this->imagen;
+
+		}
+
+		public function setImagen($imagen){
+
+			$this->imagen = $imagen;
+
+		}
+
+
+
+
+
+	}
+
+ ?>
